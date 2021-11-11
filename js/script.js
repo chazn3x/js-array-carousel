5574c6ba7030787af0e0b42634594b38e2eb192c
Giovanni <gio.a.98@gmail.com>
2021-11-11 17:37:18 +0100

    click sulle immagini e personalizzazione

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
 const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
+    'img/usa.jpeg',
+    'img/japan.jpeg',
+    'img/sweden.jpg',
+    'img/switzerland.jpeg',
+    'img/britain.jpeg',
+    'img/germany.jpeg',
+    'img/hawaii.jpeg'
 ];
 
 const title = [
+    'Stati Uniti',
+    'Giappone',
     'Svezia',
     'Svizzera',
     'Gran Bretagna',
     'Germania',
-    'Paradise'
+    'Hawaii'
 ]
 
 const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    "Gli Stati Uniti comprendono 50 stati e occupano una vasta area del Nord America, dall'Alaska a nord-ovest fino alle Hawaii nell'Oceano Pacifico.",
+    "Il Giappone è una nazione insulare nell’Oceano Pacifico con città densamente popolate, palazzi imperiali, parchi nazionali sulle montagne e migliaia di templi e santuari.",
+    "La Svezia è un paese scandinavo che comprende migliaia di isole lungo la costa e laghi nell'entroterra, oltre a vaste foreste boreali e rilievi glaciali.",
+    "La Svizzera è un paese montuoso dell'Europa Centrale dove si trovano numerosi laghi, paesini e i picchi delle Alpi.",
+    "La Gran Bretagna è un'isola separata dal continente europeo dal canale della Manica e dal mare del Nord. L’isola comprende le nazioni di Inghilterra, Scozia e Galles.",
+    "La Germania è un Paese dell'Europa Occidentale con oltre due millenni di storia e un paesaggio caratterizzato da foreste, fiumi, catene montuose e spiagge affacciate sul Mare del Nord.",
+    "Le Hawaii, uno stato degli USA, sono un arcipelago vulcanico isolato nel Pacifico centrale.",
 ]
+
+let background = document.querySelector(".background");
+background.innerHTML = `<img src="${items[0]}" alt="${title[0]} background">`;
+
+// creazione immagini grandi
 
 const previewContainer = document.querySelector(".image-container");
 // ciclo per inserire le immagini grandi
@@ -39,6 +50,8 @@ for (let i = 0; i < items.length; i++) {
 }
 document.querySelector(".preview").className = "preview active";
 
+// creazione thumbnails
+
 const carouselContainer = document.querySelector(".carousel");
 // ciclo per inserire le thumbnails
 for (let i = 0; i < items.length; i++) {
@@ -46,45 +59,56 @@ for (let i = 0; i < items.length; i++) {
     <!-- thumbnail -->
     <div class="thumbnail">
         <img src="${items[i]}" alt="${title[i]} thumbnail">
+        <h3>${title[i]}</h3>
     </div>`;
     carouselContainer.innerHTML += thumbnail;
 }
 document.querySelector(".thumbnail").className = "thumbnail active";
  
+// pulsante next
 
 let carousel = document.getElementsByClassName("thumbnail"); //array con le thumbnails
 let preview = document.getElementsByClassName("preview"); //array con le preview grandi
 let counter = 0; //contatore per thumbnails e preview
 const next = document.querySelector(".next");
 next.addEventListener("click", function() {
+    carousel[counter].className = "thumbnail";
+    preview[counter].className = "preview";
     if (counter + 1 < carousel.length) { //condizione per andare avanti
-        carousel[counter].className = "thumbnail";
-        preview[counter].className = "preview"
         counter ++;
-        carousel[counter].className = "thumbnail active";
-        preview[counter].className = "preview active"
     } else { //condizione per rendere il ciclo infinito
-        carousel[counter].className = "thumbnail";
-        preview[counter].className = "preview"
         counter = 0;
-        carousel[counter].className = "thumbnail active";
-        preview[counter].className = "preview active"
     }
+    carousel[counter].className = "thumbnail active";
+    preview[counter].className = "preview active";
+    background.innerHTML = `<img src="${items[counter]}" alt="${title[counter]} background">`;
 });
+
+// pulsante previous
 
 const prev = document.querySelector(".prev");
 prev.addEventListener("click", function() {
+    carousel[counter].className = "thumbnail";
+    preview[counter].className = "preview"
     if (counter > 0) { //condizione per andare indietro
-        carousel[counter].className = "thumbnail";
-        preview[counter].className = "preview"
         counter --;
-        carousel[counter].className = "thumbnail active";
-        preview[counter].className = "preview active"
     } else { //condizione per rendere il ciclo infinito
+        counter = carousel.length - 1;
+    }
+    carousel[counter].className = "thumbnail active";
+    preview[counter].className = "preview active";
+    background.innerHTML = `<img src="${items[counter]}" alt="${title[counter]} background">`;
+});
+
+// click sulle immagini
+
+for (let i = 0; i < carousel.length; i++) {
+    carousel[i].addEventListener("click", function() {
         carousel[counter].className = "thumbnail";
         preview[counter].className = "preview"
-        counter = carousel.length - 1;
+        counter = i;
         carousel[counter].className = "thumbnail active";
-        preview[counter].className = "preview active"
-    }
-});
+        preview[counter].className = "preview active";
+        background.innerHTML = `<img src="${items[counter]}" alt="${title[counter]} background">`;
+    });
+}
