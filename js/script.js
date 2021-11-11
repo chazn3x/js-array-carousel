bd4b929f9ef5bde99d64a2818f3a9848a2fffd96
Giovanni <gio.a.98@gmail.com>
2021-11-11 14:55:17 +0100

    fine milestone 2

--- js/script.js
+++ js/script.js
@@ -21,3 +21,20 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+const previewContainer = document.querySelector(".image-container");
+// ciclo per inserire le immagini nel container
+for (let i = 0; i < items.length; i++) {
+    let preview = `
+    <!-- immagine -->
+            <div class="preview">
+                <img src="${items[i]}" alt="Svezia">
+                <div class="layover"></div>
+                <div class="description">
+                    <h2>${title[i]}</h2>
+                    <p>${text[i]}</p>
+                </div>
+            </div>`;
+    previewContainer.innerHTML += preview;
+}
+document.querySelector(".preview").className = "preview active";
